27806ab24c5b583435b7228aa5c398d266ac38db
Kiwi-Co <kevinbinoj14@gmail.com>
2024-02-15 18:55:13 +0530

    Added more binds to board_canvas

--- src/ui/offlineNewGame.py
+++ src/ui/offlineNewGame.py
@@ -11,6 +11,7 @@ class OfflineNewGame(ctk.CTkFrame):
         self.board_canvas = ctk.CTkCanvas(self)
         self.board_canvas.grid(row=0, column=0)
         self.board_canvas.bind("<Configure>", self.createBoard)
+        self.board_canvas.bind("<ButtonRelease-1>", self.createBoard)
 
         self.side_bar = ctk.CTkFrame(self)
         self.side_bar.grid(row=0, column=1, sticky="news")
@@ -21,14 +22,14 @@ class OfflineNewGame(ctk.CTkFrame):
     def quitFrame(self): #add confirmation box
         self.root.setActivePage("Home", "Home")
         self.root.deleteFrame("OfflineNewGame")
-        # self.root.initFrame("OfflineNewGame")
         self.root.showFrame("Home")
     
     def updateSize(self):
         size = min(abs(self.winfo_width() - self.side_bar.winfo_width()), self.winfo_height())
         self.board_canvas.config(height=size, width=size)
+        return size
 
     def createBoard(self, e):
         self.board_canvas.delete("all")
-        self.updateSize()
+        size = self.updateSize()
         
